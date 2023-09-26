87462fdc75951395b706ab5732393e9d89a7cfcf
Noa Thouard <noa.thouard@nexxis.com>
2023-09-26 10:16:59 +1000

    added style sheet

--- rqt_gui/src/rqt_gui/main.py
+++ rqt_gui/src/rqt_gui/main.py
@@ -68,11 +68,34 @@ the <b>Plugins</b> menu.</p>
     def create_application(self, argv):
         from python_qt_binding.QtGui import QIcon
         app = super(Main, self).create_application(argv)
+        self.apply_nexxis_style_sheet(app)
         rqt_gui_path = get_package_path('rqt_gui')
         logo = os.path.join(rqt_gui_path, 'share', 'rqt_gui', 'resource', 'rqt.png')
         icon = QIcon(logo)
         app.setWindowIcon(icon)
+        app.aboutToQuit.connect(self.closeEvent)
         return app
+    
+    def apply_nexxis_style_sheet(self, app):
+        app.setStyleSheet(
+        "QMainWindow { background-color: #302B2D }" "QMainWindow { alternate-background-color: #3a3536 }" "QMainWindow { border-color: #211f1f }" "QMainWindow { color: #bdbdbd }"     
+        "QDockWidget { background-color: #302B2D }" "QDockWidget { alternate-background-color: #3a3536 }" "QDockWidget { color: #bdbdbd }"
+        "QWidget { background-color: #302B2D }" "QWidget { alternate-background-color: #3a3536 }" "QWidget { color: #bdbdbd }"
+        "QFrame { background-color: #302B2D }" "QFrame { alternate-background-color: #3a3536 }" "QFrame { selection-background-color: #302B2D }" "QFrame { border-color: #211f1f }" "QFrame { color: #bdbdbd }"
+        "QGroupBox { background-color: #302B2D }" "QGroupBox { alternate-background-color: #3a3536 }" "QGroupBox { border-color: #211f1f }" "QGroupBox { color: #bdbdbd }"
+        "QTableView { background-color: #3a3536 }" "QTableView { selection-background-color: #6b6969 }" "QTableView { alternate-background-color: #3a3536 }" "QTableView { color: #bdbdbd }"
+        
+        "QLineEdit { background-color: #3a3536 }" "QLineEdit { alternate-background-color: #3a3536 }" "QLineEdit { border-color: #211f1f }" "QLineEdit { color: #bdbdbd }"
+        "QDialog { background-color: #302B2D }" "QDialog { alternate-background-color: #3a3536 }" "QDialog { border-color: #211f1f }" "QDialog { color: #bdbdbd }"
+        "QScrollBar { background-color: #3a3536 }" "QScrollBar { alternate-background-color: #3a3536 }" "QScrollBar { border-color: #211f1f }" "QScrollBar { color: #bdbdbd }"
+        "QCheckBox { background-color: #3a3536 }" "QCheckBox { alternate-background-color: #3a3536 }" "QCheckBox { color: #bdbdbd }"
+
+        "QMenu { background-color: #3a3536 }" "QMenu { border-color: #211f1f }" "QMenu { color: #bdbdbd }"
+        "QMenu { selection-background-color: #6b6969 }" "QMenu { selection-color: white }"
+        "QMenuBar { background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #4a4243, stop: 0.3 #3a3536, stop: 0.9 #3a3536, stop:1 #023B2D) }" "QMenuBar { color: #bdbdbd }"
+        "QMenuBar { border: 1px solid #211f1f }" "QMenuBar { border-top: 1px solid #4a4243 }"
+        "QMenuBar { selection-background-color: #6b6969 }" "QMenuBar { selection-color: white }"
+        )
 
     def _add_plugin_providers(self):
         # do not import earlier as it would import Qt stuff without the proper
@@ -89,6 +112,11 @@ the <b>Plugins</b> menu.</p>
         super(Main, self)._add_reload_paths(reload_importer)
         reload_importer.add_reload_path(os.path.join(os.path.dirname(__file__), *('..',) * 4))
 
+    def closeEvent(self):
+        print('Shutting down Magneto')
+        import signal
+        os.kill(os.getppid(), signal.SIGHUP)
+        sys.exit(0)
 
 def main():
     sys.exit(Main().main())
